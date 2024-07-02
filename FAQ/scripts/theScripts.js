a8db03fab5b026fdcd90db43324bdafe1846870a
Bashir Rayaneh <zakiyeh.hosseinzadeh@gmail,com>
2024-07-02 12:00:50 +0330

    [fix]: stop the accordion when the user clicked.

--- FAQ/scripts/theScripts.js
+++ FAQ/scripts/theScripts.js
@@ -7,49 +7,53 @@ $('.sec-acc-title').click(function () {
     }
     $(this).next().slideToggle('fast').siblings('.sec-acc-content').slideUp('fast')
 })
-
 $(document).ready(function () {
-    $('.myParagraph').hide();
-    $('.myParagraph[id="p3"]').show();
+    let userClick=false;
 
-})
-$('.acc-title').click(function () {
-    let index = $(this).data('answer');
-    const answer = $(this).data()
-    console.log(answer)
-
-    $('.acc-title').removeClass('active');
-    if ($(this).is(':visible')) {
-        $(this).toggleClass('active');
+    $(document).ready(function () {
         $('.myParagraph').hide();
-        $('.myParagraph[id="' + index + '"]').show();
+        $('.myParagraph[id="p3"]').show();
+
+    })
+    $('.acc-title').click(function () {
+        let index = $(this).data('answer');
+        const answer = $(this).data()
+        console.log(answer)
+
+        $('.acc-title').removeClass('active');
+        if ($(this).is(':visible')) {
+            $(this).toggleClass('active');
+            $('.myParagraph').hide();
+            $('.myParagraph[id="' + index + '"]').show();
+        }
+        userClick=true;
+        $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast');
+    })
+
+    let accordions = document.getElementsByClassName("acc-title");
+    let autoOpen;
+    let autoOpenEnabled = true;
+
+    function autoOpenAccordions() {
+        let index = 0;
+        autoOpen = setInterval(function () {
+            if (autoOpenEnabled&&!userClick) {
+                $('.acc-title').removeClass('active');
+                accordions[index].classList.add("active");
+                let accContent = accordions[index].nextElementSibling;
+                accContent.style.display = "block";
+
+                setTimeout(function () {
+                    accordions[index].classList.remove("active");
+                    accContent.style.display = "none";
+                }, 1000);
+
+                index++;
+                if (index >= accordions.length) index = 0;
+            }
+        }, 3000);
     }
 
-    $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast');
+    autoOpenAccordions();
+
 })
-
-let accordions = document.getElementsByClassName("acc-title");
-let autoOpen;
-let autoOpenEnabled = true;
-
-function autoOpenAccordions() {
-    let index = 0;
-    autoOpen = setInterval(function () {
-        if (autoOpenEnabled) {
-            $('.acc-title').removeClass('active');
-            accordions[index].classList.add("active");
-            let accContent = accordions[index].nextElementSibling;
-            accContent.style.display = "block";
-
-            setTimeout(function () {
-                accordions[index].classList.remove("active");
-                accContent.style.display = "none";
-            }, 3000);
-
-            index++;
-            if (index >= accordions.length) index = 0;
-        }
-    }, 5000);
-}
-
-autoOpenAccordions();
