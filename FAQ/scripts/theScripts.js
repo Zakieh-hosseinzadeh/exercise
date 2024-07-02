19eb4fd1fc77853bbfdd98f6a5d1f118d2f00783
Bashir Rayaneh <zakiyeh.hosseinzadeh@gmail,com>
2024-07-02 10:45:46 +0330

    [fix]: add some data-answer

--- FAQ/scripts/theScripts.js
+++ FAQ/scripts/theScripts.js
@@ -9,41 +9,44 @@ $('.sec-acc-title').click(function () {
 })
 
 $('.acc-title').click(function () {
+    let index=0;
+    
     const answer = $(this).data()
     console.log(answer)
-
     $('.acc-title').removeClass('active');
     if ($(this).is(':visible')) {
-        $(this).toggleClass('active')
+        $(this).toggleClass('active');
+        $('.myParagraph').hide();
+        $('.myParagraph[id="p2"]').show();
     }
-    $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast')
+    $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast');
 
 })
 
-let accordions = document.getElementsByClassName("acc-title");
-let autoOpen;
-let autoOpenEnabled = true;
+// let accordions = document.getElementsByClassName("acc-title");
+// let autoOpen;
+// let autoOpenEnabled = true;
+//
+// function autoOpenAccordions() {
+//     let index = 0;
+//     autoOpen = setInterval(function () {
+//         if (autoOpenEnabled) {
+//             $('.acc-title').removeClass('active');
+//             accordions[index].classList.add("active");
+//             let accContent = accordions[index].nextElementSibling;
+//             accContent.style.display = "block";
+//
+//             setTimeout(function () {
+//                 accordions[index].classList.remove("active");
+//                 accContent.style.display = "none";
+//                 $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast')
+//
+//             }, 2000);
+//
+//             index++;
+//             if (index >= accordions.length) index = 0;
+//         }
+//     }, 5000);
+// }
 
-function autoOpenAccordions() {
-    let index = 0;
-    autoOpen = setInterval(function () {
-        if (autoOpenEnabled) {
-            $('.acc-title').removeClass('active');
-            accordions[index].classList.add("active");
-            let accContent = accordions[index].nextElementSibling;
-            accContent.style.display = "block";
-
-            setTimeout(function () {
-                accordions[index].classList.remove("active");
-                accContent.style.display = "none";
-                $(this).next().slideToggle('fast').siblings('.acc-content').slideUp('fast')
-
-            }, 2000);
-
-            index++;
-            if (index >= accordions.length) index = 0;
-        }
-    }, 5000);
-}
-
-autoOpenAccordions();
+// autoOpenAccordions();
